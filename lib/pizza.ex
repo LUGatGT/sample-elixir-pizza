a845366b1f793c1bcd3692b1a7eb28ca0bd34882
Michael Eden <themichaeleden@gmail.com>
2017-03-15 16:06:01 -0400

    added tcp server

--- lib/pizza.ex
+++ lib/pizza.ex
@@ -1,20 +1,9 @@
 defmodule Pizza do
-    @doc "start our process"
-    def start do
-        Task.start_link(&loop/0)
-    end
-
     def is_pizza_week time do
-        weeks_since = (time - magic()) / (1000 * 60 * 60 * 24 * 7)
+        weeks_since = (time - magic()) / (60 * 60 * 24 * 7)
         (rem (trunc weeks_since), 2) === 1
     end
 
-    defp loop do
-        receive do
-            {:get, caller} -> {:pizza, (send caller, ((System.system_time / 1000000) |> trunc |> is_pizza_week))}
-        end
-    end
-
-    defp magic, do: 1476322937348
+    defp magic, do: 1476322937
 end
 
