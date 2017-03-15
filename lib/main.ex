f9a5a0dd09b389c4540e1cef791777eb654dfb11
Michael Eden <themichaeleden@gmail.com>
2017-03-15 16:30:59 -0400

    added server supervisor

--- lib/main.ex
+++ lib/main.ex
@@ -4,7 +4,17 @@ defmodule Main do
   use Application
 
   def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Task, [Server, :accept, [8080]])
+    ]
+    opts = [
+      strategy: :one_for_one,
+      name: Server.Supervisor,
+    ]
+
     Logger.info "Starting app..."
-    Server.accept 8080
+    Supervisor.start_link(children, opts)
   end
 end
